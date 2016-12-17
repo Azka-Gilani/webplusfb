06901c6b46944c178acd1e0dbe7a832f6257d8dc
Azka-Gilani <azka.gilani@gmail.com>
2016-12-17 12:52:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -305,12 +305,70 @@ def makeWebhookResult(data):
         ]
   }
     else:
-        message= {
-         "text":"Just put some vinegar on it", 
-            "role": "appMaker",
-            "type": "text",
-            "actions": [{"type": "link", "text": "Put vinegar", "uri": "http://example.com" }]
-        
+         message= {
+         "attachment": {
+           "type": "template",
+            "payload": {
+               "template_type": "generic",
+               "elements": [{
+               "title": row_title[0],
+               "subtitle": row_location[0],
+               "item_url": "https://www.aarz.pk/property-detail/"+row_slug[0],               
+               "image_url": "https://www.aarz.pk/assets/images/properties/"+row_id[0]+"/"+row_id[0]+".actual.0.jpg" ,
+                "buttons": [{
+                "type": "web_url",
+                "url": "www.aarz.pk",
+                "title": "Open Web URL"
+            }, 
+                   ],
+          }, 
+                   {
+                "title": row_title[1],
+                "subtitle": row_location[1],
+                "item_url":  "http://www.aarz.pk/property-detail?id="+row_id[1],               
+                "image_url": "http://www.aarz.pk/assets/images/properties/"+row_id[1]+"/"+row_id[1]+".actual.0.jpg",
+                "buttons": [{
+                "type": "web_url",
+                "url": "www.aarz.pk",
+                "title": "Open Web URL"
+            },
+                   ]
+          }]
+            
+        }
+      },
+             "quick_replies": [
+            {
+                "content_type":"text",
+                "title": QR[0],
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            },
+                 {
+                "content_type":"text",
+                "title": QR[1],
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            },
+                 {
+                "content_type":"text",
+                "title": QR[2],
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            },
+                 {
+                "content_type":"text",
+                "title": QR[3],
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            },
+                 {
+                "content_type":"text",
+                "title": QR[4],
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            },
+                 {
+                "content_type":"text",
+                "title": "Purchase plot",
+                "payload": "YOUR_DEFINED_PAYLOAD_FOR_NEXT_IMAGE"
+            }
+        ]
     }
     return {
         "speech": speech,
